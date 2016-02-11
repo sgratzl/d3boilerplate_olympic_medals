3081608324101851fcb991bbb9ac143f745f4d7b
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-02-11 17:35:53 +0100

    add medal data

--- main.js
+++ main.js
@@ -2,40 +2,49 @@
  * Created by Samuel Gratzl on 11.02.2016.
  */
 
-var svg = d3.select('body').append('svg').attr({
-  width: 300,
-  height: 300
-});
+var table1 = d3.select('body').append('table');
+table1.append('thead').append('tr');
+table1.append('tbody');
+var table2 = d3.select('body').append('table');
+table2.append('thead').append('tr');
+table2.append('tbody');
 
-var xscale = d3.scale.linear()
-      .range([0,300]);
+var result_formatter = d3.time.format.utc('%H:%M:%S.%L');
 
-var yscale = d3.scale.ordinal()
-      .rangeRoundBands([0, 300], 0.1, 0.1);
+function update(table, data) {
+  var data_columns = Object.keys(data[0]);
 
+  var columns = table.select('thead tr').selectAll('th').data(data_columns);
+  columns.enter().append('th');
+  columns.text(String);
+  columns.exit().remove();
 
-function update(data) {
-
-  xscale.domain([0, d3.max(data)]);
-  yscale.domain(d3.range(data.length));
-
-  var rects = svg.selectAll('rect').data(data);
-  rects.enter().append('rect');
-  rects.attr({
-    x : 0,
-    y : function(d, i) { return yscale(i); },
-    width: function(d) { return xscale(d); },
-    height: yscale.rangeBand
+  var rows = table.select('tbody').selectAll('tr').data(data);
+  rows.enter().append('tr');
+  var cols = rows.selectAll('td').data(function(row) { return data_columns.map(function(d) { return row[d]; })});
+  cols.enter().append('td');
+  cols.text(function(d,i) {
+    if (d instanceof Date) {
+      return result_formatter(d);
+    } else {
+      return d;
+    };
   });
-  rects.exit().remove();
+  cols.exit().remove();
+
+  rows.exit().remove();
 }
 
-//var data = [3, 4, 2, 5, 7];
-//update(data);  
-
 //external data
-d3.csv('./data/dataset.csv', function(csv) {
-  var data = csv.map(function(d) { return d.value; });
-  update(data);  
-})
+d3.tsv('./data/MedalData1.csv', function(metaldata1) {
+  metaldata1.forEach(function(d) {
+     //convert to date
+    d.Result = d.ResultInSeconds === 'No result' ? null : new Date(parseFloat(d.ResultInSeconds)*1000);
+    delete d.ResultInSeconds;
+  });
+  update(table1, metaldata1.slice(0,10));
+});
 
+d3.tsv('./data/MedalData2.csv', function(metaldata2) {
+  update(table2, metaldata2.slice(0,10));
+});
